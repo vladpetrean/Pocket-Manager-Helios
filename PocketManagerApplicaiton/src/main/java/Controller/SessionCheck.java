18f597c11a95c515cdbd759e0262bef5038861ae
FlorinSingeorzan <florin.singeorzan96@gmail.com>
2019-04-03 17:02:23 +0300

    session check

---
+++ PocketManagerApplicaiton/src/main/java/Controller/SessionCheck.java
@@ -0,0 +1,31 @@
+package Controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class SessionCheck {
+    public static void setSessionUser(HttpServletRequest request, String attributeValue){
+        request.setAttribute("user",attributeValue);  ////la apel attributeValue este ADMIN sau USER
+
+    }
+//    <%
+//    String type= (String) request.getAttribute("user");
+//    if(type!=null && type.equals("USER")){
+//
+//    }%>
+//    --pagina--
+//    <%
+//}else{
+//
+//
+//        %>
+//    <h3 class="text-center">Not Allowed </h3>
+//    <h1 class="text-center">404</h1>
+//
+//
+//    <%
+//        }
+//        %>
+
+
+
+    }
