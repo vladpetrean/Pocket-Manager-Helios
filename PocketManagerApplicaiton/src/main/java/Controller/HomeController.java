67df2e8a7e586a617288e85c9e27798700f6aefb
Vlad <vladimir_daniel@yahoo.com>
2019-04-03 16:33:35 +0300

    Implement log in.

--- PocketManagerApplicaiton/src/main/java/Controller/HomeController.java
+++ PocketManagerApplicaiton/src/main/java/Controller/HomeController.java
@@ -4,24 +4,46 @@ package Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import java.util.ArrayList;
 
 import Model.Account;
 import Configuration.DatabseOperation;
 
+import javax.servlet.http.HttpServletRequest;
+
 
 @Controller
 public class HomeController {
-    @GetMapping({"/", "/hello"})
-    public String hello(Model model, @RequestParam(value = "name", required = false, defaultValue = "World") String name) {
-
-        ArrayList<Account> accountArrayList = DatabseOperation.obtainAdministrator();
-        for(Account account: accountArrayList){
-            System.out.println(account.getAccountName());
-        }
-        name = "Test printare variabila template";
-        model.addAttribute("name", name);
-        return "hello";
+    @GetMapping({"/index"})
+    public String sign_in_get(Model model, HttpServletRequest request) {
+        return "index";
     }
+
+    @PostMapping({"/index"})
+    public String sign_in_post(Model model, HttpServletRequest request) {
+
+        String password = request.getParameter("userPassword");
+        String username = request.getParameter("userName");
+
+        if(password != null && username != null){
+            System.out.println(password);
+            System.out.println(username);
+            if(check_sign_in(password, username)){
+                FlorinMethod();
+                return "hello";
+            }
+            else{
+                model.addAttribute("message", "Login failed");
+                return "hello";
+            }
+        }
+        else{
+            model.addAttribute("message", "Login failed");
+            return "hello";
+        }
+    }
+
+
 }
